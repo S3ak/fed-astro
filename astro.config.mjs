9bb505a03cac5122272259118bda4f6791209c4f
Monde Sineke <nsineke@gmail.com>
2023-01-27 17:28:18 +0100

    feat: Add examples

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
@@ -8,9 +8,11 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-  // Enable Preact to support Preact JSX components.
-  preact(),
-  // Enable React for the Algolia search component.
-  react(), mdx()],
-  site: `http://astro.build`
+    // Enable Preact to support Preact JSX components.
+    preact(),
+    // Enable React for the Algolia search component.
+    react(),
+    mdx(),
+  ],
+  site: `http://astro.build`,
 });
